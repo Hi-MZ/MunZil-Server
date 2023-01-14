9ec2ba1380d2cffcd10cd7eb45b9c6b11320523c
Sangjin <sangjin170202@naver.com>
2023-01-14 23:28:19 +0900

    add(#1): Feed 추가

---
+++ src/main/java/io/munzil/munzil/domain/feed/domain/Feed.java
@@ -0,0 +1,49 @@
+package io.munzil.munzil.domain.feed.domain;
+
+import io.munzil.munzil.domain.like.domain.FeedLike;
+import io.munzil.munzil.domain.user.domain.User;
+import io.munzil.munzil.global.entity.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tbl_feed")
+public class Feed extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.REMOVE)
+    private List<FeedLike> feedLikes;
+
+    @OneToMany(mappedBy = "feed", cascade = CascadeType.REMOVE)
+    private List<FeedImage> feedImages;
+
+    @Builder
+    public Feed(String content, User user) {
+        this.content = content;
+        this.user = user;
+    }
+
+    public void updateFeed(String content) {
+        this.content = content;
+    }
+
+}
